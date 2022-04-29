8fa7b03eff536d4635aaaa35611111fb38615e76
ghxops <51935312+ghxops@users.noreply.github.com>
2022-04-29 14:57:22 +0000

    style: formatting code with Autopep8

--- src/task.py
+++ src/task.py
@@ -1,6 +1,7 @@
 from task_interfaces import WorkflowTask, SubscriptionLevels
 from typing import List, Dict
 
+
 class Task(WorkflowTask):
     """
     Checks Pull Requests are manageable for review.
@@ -15,14 +16,13 @@ class Task(WorkflowTask):
         }
     ]
 
-
     def execute(self, github_body, settings) -> bool:
         # A manual override has been requested
         if (
-            github_body.get("githaxs", {}).get("full_event")
-            == "check_run.requested_action"
-            and github_body.get("requested_action", {}).get("identifier", "")
-            == "override"
+            github_body.get("githaxs", {}).get("full_event") ==
+            "check_run.requested_action" and
+            github_body.get("requested_action", {}).get("identifier", "") ==
+            "override"
         ):
             self.actions = None
             self.pass_summary = (
@@ -33,12 +33,12 @@ class Task(WorkflowTask):
 
         # Normal use case
         status = (
-            github_body.get("pull_request", {}).get("changed_files", 0)
-            <= (int(settings.get("max_changed_files", "0")) or float("inf"))
-            and github_body.get("pull_request", {}).get("additions", 0)
-            <= (int(settings.get("max_additions", "0")) or float("inf"))
-            and github_body.get("pull_request", {}).get("deletions", 0)
-            <= (int(settings.get("max_deletions", "0")) or float("inf"))
+            github_body.get("pull_request", {}).get("changed_files", 0) <=
+            (int(settings.get("max_changed_files", "0")) or float("inf")) and
+            github_body.get("pull_request", {}).get("additions", 0) <=
+            (int(settings.get("max_additions", "0")) or float("inf")) and
+            github_body.get("pull_request", {}).get("deletions", 0) <=
+            (int(settings.get("max_deletions", "0")) or float("inf"))
         )
 
         if status:
