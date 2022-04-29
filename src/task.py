78ca3caa888538eac65d1b51e344ff999ef73c91
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-04-29 11:03:04 -0400

    feat: simplify task interface (#5)

--- src/task.py
+++ src/task.py
@@ -1,20 +1,14 @@
-from task_interfaces import SubscriptionLevels
-from task_interfaces import TaskInterface
-from task_interfaces import TaskTypes
+from task_interfaces import WorkflowTask, SubscriptionLevels
+from typing import List, Dict
 
 
-class Task(TaskInterface):
+class Task(WorkflowTask):
     """
     Checks Pull Requests are manageable for review.
     """
-
-    name = "PR Too Big"
-    slug = "pr-too-big"
-    pass_text = ""
-    fail_text = ""
-    fail_summary = "This Pull Request has too many changes."
-    subscription_level = SubscriptionLevels.FREE
-    actions = [
+    name: str = "PR Too Big"
+    subscription_level: int = SubscriptionLevels.FREE
+    actions: List[Dict] = [
         {
             "label": "Override",
             "identifier": "override",
@@ -22,44 +16,32 @@ class Task(TaskInterface):
         }
     ]
 
-    type = TaskTypes.WORKFLOW
-
     def execute(self, github_body, settings) -> bool:
-        self.pass_summary = ""
-
         # A manual override has been requested
         if (
-            github_body.get("githaxs", {}).get("full_event")
-            == "check_run.requested_action"
-            and github_body.get("requested_action", {}).get("identifier", "")
-            == "override"
+            github_body.get("githaxs", {}).get("full_event") ==
+            "check_run.requested_action" and
+            github_body.get("requested_action", {}).get("identifier", "") ==
+            "override"
         ):
             self.actions = None
             self.pass_summary = (
                 "%s has overridden the original result"
                 % github_body.get("sender").get("login")
             )
-            return True
+            return self.pass_summary
 
         # Normal use case
         status = (
-            github_body.get("pull_request", {}).get("changed_files", 0)
-            <= (int(settings.get("max_changed_files", "0")) or float("inf"))
-            and github_body.get("pull_request", {}).get("additions", 0)
-            <= (int(settings.get("max_additions", "0")) or float("inf"))
-            and github_body.get("pull_request", {}).get("deletions", 0)
-            <= (int(settings.get("max_deletions", "0")) or float("inf"))
+            github_body.get("pull_request", {}).get("changed_files", 0) <=
+            (int(settings.get("max_changed_files", "0")) or float("inf")) and
+            github_body.get("pull_request", {}).get("additions", 0) <=
+            (int(settings.get("max_additions", "0")) or float("inf")) and
+            github_body.get("pull_request", {}).get("deletions", 0) <=
+            (int(settings.get("max_deletions", "0")) or float("inf"))
         )
 
         if status:
             self.actions = None
 
         return status
-
-    @property
-    def pass_summary(self) -> str:
-        return self._pass_summary
-
-    @pass_summary.setter
-    def pass_summary(self, pass_summary):
-        self._pass_summary = pass_summary
